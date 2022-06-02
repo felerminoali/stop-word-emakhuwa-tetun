f6c5d305f7da78bfa7310dbe9ca99a4efaa11514
Gabriel <timornewstl@gmail.com>
2022-06-02 18:50:22 +0100

    update tokenizer

--- edges.py
+++ edges.py
@@ -10,15 +10,30 @@ def load_data(file_path):
   return data
 
 
+# lowercasing and tokenize
+def tetun_tokenizer(data_source):
+    data = load_data(data_source)
+    lower_data = data.lower() #lowercase the corpus
+    tokens = re.findall(r"[íéêóáú]*[ÁÉÊÍÓÚ]*[A-Za-z]+[-’'íéêáóúñ]*[ÁÉÍêÓÚ]*[A-Za-z]*[-’'íéêáóú]*[A-Za-z]*", lower_data)
+
+    return tokens
+
+
 # function to generate edges using 2 grams as default value
 def generate_edges(file, data_source, n=2, delimiter=" ", newline="\n", weight=1):
-    data = load_data(data_source)
-    tokens = re.findall(r"[íéêóáú]*[ÁÉÊÍÓÚ]*[A-Za-z0-9]+[-’'íéêáóúñ]*[ÁÉÍêÓÚ]*[A-Za-z0-9]*[-’'íéêáóú]*[a-z0-9]*", data) 
+    tokens = tetun_tokenizer(data_source)
     two_grams = list(ngrams(tokens, 2))
 
     file = open(file, "w")
     for two_gram in two_grams:
         gram_1, gram_2 = two_gram
+        
+        if gram_1[-1] == "-":
+            gram_1 = gram_1[:-1] #remove "-" if it is the last char of word
+        
+        if gram_2[-1] == "-":
+            gram_2 = gram_2[:-1] #remove "-" if it is the last char of word
+
         file.writelines(gram_1 + delimiter + gram_2 + delimiter + str(weight) + newline)
     file.close()
 
