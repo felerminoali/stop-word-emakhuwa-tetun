9b40387595dc40504b2081a98a507f193b023fca
felerminoali <felerminoali@gmail.com>
2022-06-04 15:46:53 +0100

    gephi data generated

--- edges.py
+++ edges.py
@@ -1,12 +1,12 @@
 from nltk import ngrams
+from nltk.tokenize import word_tokenize
 import re
-
+from nltk import tokenize
 
 # function to load data
 def load_data(file_path):
-  with open(file_path, 'r') as f:
+  with open(file_path, 'r', encoding="utf8") as f:
     data = f.read()
-
   return data
 
 
@@ -18,19 +18,27 @@ def tetun_tokenizer(data_source):
 
     return tokens
 
+def portuguese_tokenizer(data_source):
+    data = load_data(data_source)
+    lower_data = data.lower()  # lowercase the corpus
+    tokens = [token for token in tokenize.word_tokenize(lower_data, language='portuguese') if token.isalpha()]
+    # print(tokens)
+    return tokens
 
 # function to generate edges using 2 grams as default value
-def generate_edges(file, data_source, n=2, delimiter=" ", newline="\n"):
-    tokens = tetun_tokenizer(data_source)
+def generate_edges(file, data_source, n=2, delimiter=", ", newline="\n"):
+    # tokens = tetun_tokenizer(data_source)
+    tokens = portuguese_tokenizer(data_source)
+
     two_grams = list(ngrams(tokens, 2))
 
-    file = open(file, "w")
+    file = open(file, "w", encoding="utf8")
     for two_gram in two_grams:
         gram_1, gram_2 = two_gram
         
         if gram_1[-1] == "-":
             gram_1 = gram_1[:-1] #remove "-" if it is the last char of word
-        
+
         if gram_2[-1] == "-":
             gram_2 = gram_2[:-1] #remove "-" if it is the last char of word
 
@@ -38,4 +46,5 @@ def generate_edges(file, data_source, n=2, delimiter=" ", newline="\n"):
     file.close()
 
 # generate the file for edges
-generate_edges("files/edges.csv", "files/tetun.txt")
+lang='portuguese'
+generate_edges("files/edges-"+lang+".csv", "files/"+lang+".txt")
--- node.py
+++ node.py
@@ -1,6 +1,10 @@
 import os
 import re
 import csv
+from nltk.corpus import stopwords
+
+from nltk import tokenize
+
 
 def load_data_as_dict(path):
     lines = []
@@ -15,15 +19,14 @@ def load_data(file_path):
   return data
 
 # lowercasing and tokenize
-def tetun_tokenizer(data_source):
-    data = load_data(data_source)
+def tetun_tokenizer(data):
     lower_data = data.lower() #lowercase the corpus
     tokens = re.findall(r"[íéêóáú]*[ÁÉÊÍÓÚ]*[A-Za-z]+[-’'íéêáóúñ]*[ÁÉÍêÓÚ]*[A-Za-z]*[-’'íéêáóú]*[A-Za-z]*", lower_data)
     return tokens
 
-def tetun_tokenizer_data(data):
-    lower_data = data.lower() #lowercase the corpus
-    tokens = re.findall(r"[íéêóáú]*[ÁÉÊÍÓÚ]*[A-Za-z]+[-’'íéêáóúñ]*[ÁÉÍêÓÚ]*[A-Za-z]*[-’'íéêáóú]*[A-Za-z]*", lower_data)
+def portuguese_tokenizer(data):
+    lower_data = data.lower()  # lowercase the corpus
+    tokens = [token for token in tokenize.word_tokenize(lower_data, language='portuguese') if token.isalpha()]
     return tokens
 
 def count_freq(term, tokens):
@@ -36,14 +39,33 @@ def count_freq(term, tokens):
                 break
     return sum
 
-def build_vocabulary(data):
-    tokens = tetun_tokenizer(data)
+def tokenize_func(lang, data_source):
+    tokens = []
+    data = load_data(data_source)
+    if lang == 'tetun':
+        tokens = tetun_tokenizer(data)
+    elif lang == 'portuguese':
+        tokens = portuguese_tokenizer(data)
+    return tokens
+
+def tokenize_data(lang, data):
+    tokens = []
+    if lang == 'tetun':
+        tokens = tetun_tokenizer(data)
+    elif lang == 'portuguese':
+        tokens = portuguese_tokenizer(data)
+    return tokens
+
+def build_vocabulary(lang, data_path):
+    # tokens = tetun_tokenizer(data)
+    tokens = tokenize_func(lang, data_path)
     vocabulary = list(sorted(set(tokens)))
     return vocabulary
 
 # number of ocurrency
-def term_frequency(path):
-    tokens = sorted(tetun_tokenizer(path))
+def term_frequency(lang, data_path):
+    # tokens = sorted(tetun_tokenizer(path))
+    tokens = sorted(tokenize_func(lang, data_path))
     tf = {}
     top = tokens.pop()
     count = 0
@@ -58,19 +80,21 @@ def term_frequency(path):
     tf[top] = count + 1
     return tf
 
-def document_frequency(path):
+def document_frequency(lang, path):
     file = load_data_as_dict(path)
 
     list_tokens = []
     for line in file:
-        tokens = re.findall(r"[íéêóáú]*[ÁÉÊÍÓÚ]*[A-Za-z]+[-’'íéêáóúñ]*[ÁÉÍêÓÚ]*[A-Za-z]*[-’'íéêáóú]*[A-Za-z]*", line.lower())
+        tokens = tokenize_data(lang, line)
         uniques = list(set(tokens))
         list_tokens.extend(uniques)
 
-
+    list_tokens = sorted(list_tokens)
+    print(list_tokens)
     df = {}
     top = list_tokens.pop()
     count = 0
+
     while len(list_tokens) > 0:
         current = list_tokens.pop()
         if current == top:
@@ -83,51 +107,51 @@ def document_frequency(path):
     return df
 
 if __name__ == '__main__':
-    path = 'files/tetun.txt'
+    lang = 'portuguese'
+    path = 'files/'+lang+'.txt'
 
-    tf = term_frequency(path)
+    tf = term_frequency(lang, path)
     print(len(tf))
     print(tf)
 
-    df = document_frequency(path)
+    df = document_frequency(lang, path)
     print(len(df))
     print(df)
 
-    df = document_frequency(path)
-    print(len(df))
-    print(df)
-
-    print(df.keys())
-
-    vocabulary = build_vocabulary(path)
+    vocabulary = build_vocabulary(lang, path)
 
     print(len(vocabulary))
 
-    file = open('files/nodes.csv', "w", encoding="utf8")
+    file = open('files/nodes-'+lang+'.csv', "w", encoding="utf8")
     delimiter = ","
     newline = '\n'
-    file.writelines("id" + delimiter + "Label"+ delimiter + "tf" + delimiter + "df" + delimiter + "tdf" + newline)
+    file.writelines("id" + delimiter + "Label"+ delimiter + "tf" + delimiter + "df" + delimiter + "tdf" + delimiter +"stopword"+ newline)
+
+    print(stopwords.words('portuguese'))
+
+    stopw = stopwords.words('portuguese') if lang == 'portuguese' else []
+    print(len(stopw))
 
     ids = {}
     for index, term in enumerate(df.keys()):
         ids[term] = index
-        file.writelines(str(index) + delimiter + term + delimiter + str(tf[term]) + delimiter + str(df[term]) + delimiter + str(tf[term]*df[term]) + newline)
-        print("{term} {tf} {df} {tdf}".format(term=term, tf=tf[term], df=df[term], tdf=tf[term]*df[term]))
+
+        is_stop = '1' if term in stopw else '0'
+        file.writelines(str(index) + delimiter + term + delimiter + str(tf[term]) + delimiter + str(df[term]) + delimiter + str(tf[term]*df[term]) + delimiter+ is_stop+ newline)
+        # print("{term} {tf} {df} {tdf}".format(term=term, tf=tf[term], df=df[term], tdf=tf[term]*df[term]))
 
     print(ids)
 
     out = ["Source, Target, Type, Weight"]
-    with open('files/edges.csv', 'r', encoding="utf8") as csvfile:
+    with open('files/edges-'+lang+'.csv', 'r', encoding="utf8") as csvfile:
         reader = csv.reader(csvfile)
         header = next(reader)
         for row in reader:
-            # print(row)
+            print(row)
             # print(str(ids[row[0].strip()]) + ', '+str(ids[row[1].strip()]))
             out.append(str(ids[row[0].strip()]) + ', '+str(ids[row[1].strip()])+', Directed, 1.0')
 
-    file2 = open("files/edges-gephi.csv", "w", encoding="utf8")
+    file2 = open("files/edges-"+lang+"-gephi.csv", "w", encoding="utf8")
     file2.writelines("\n".join(out))
     file2.close()
 
-    # for id in ids.keys():
-    #     print(id+" -- id: "+str(ids[id]) )
