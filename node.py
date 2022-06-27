17208ec22da71ec9023ba5667468caac3afa8f3f
felerminoali <felerminoali@gmail.com>
2022-06-27 15:54:58 +0100

    normalized

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-from config import term_frequency, document_frequency, remove_noise
+from config import load_data_as_dict, term_frequency, document_frequency, remove_noise, normalized_term_frequency, idf, normalized_idf
 from nltk.corpus import stopwords
 import csv
 
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     folder = 'wikiclir/'
     path = folder+lang+'.txt'
 
+    number_of_docs = len(load_data_as_dict(path))
 
     stopw = stopwords.words(lang)
     file = open(folder+lang+'/nodes-'+lang+'.csv', "w", encoding="utf-8")
@@ -15,21 +16,45 @@ if __name__ == '__main__':
     tf = term_frequency(lang, path)
     tf = remove_noise(tf, lang)
 
+    ntf = normalized_term_frequency(tf)
+
     df = document_frequency(lang, path)
     df = remove_noise(df, lang)
 
+    idf = idf(number_of_docs, df)
+    nidf = normalized_idf(number_of_docs, df)
 
     delimiter = ","
     newline = '\n'
     ids = {}
-    nodes = ["id" + delimiter + "Label"+ delimiter + "tf" + delimiter + "df" + delimiter + "tdf" + delimiter +"stopword"+ newline]
+    nodes = [
+            "id" + delimiter + 
+            "Label"+ delimiter + 
+            "tf" + delimiter + 
+            "df" + delimiter + 
+            "tdf" + delimiter + 
+            "tf-idf" + delimiter + 
+            "idf" + delimiter +
+            "nidf" + delimiter + 
+            "ntf" + delimiter+ 
+            "stopword" + newline
+            ]
     for index, term in enumerate(tf.keys()):
         ids[term] = index
         is_stop = '1' if term in stopw else '0'
-        nodes.append(str(index) + delimiter + term + delimiter + str(tf[term]) + delimiter + str(df[term]) + delimiter + str(tf[term]*df[term]) + delimiter+ is_stop+ newline)
+        nodes.append(
+            str(index) + delimiter + 
+            term + delimiter +
+            str(tf[term]) + delimiter + 
+            str(df[term]) + delimiter + 
+            str(tf[term]*df[term]) + delimiter  + 
+            str(ntf[term]*idf[term]) + delimiter  + 
+            str(idf[term]) + delimiter + 
+            str(nidf[term]) + delimiter + 
+            str(ntf[term]) + delimiter + 
+            is_stop+ newline)
     file.writelines(nodes)
     file.close()
-
 
     out = ["Source, Target, Type, Weight"]
     error = []
@@ -43,8 +68,6 @@ if __name__ == '__main__':
             edges.append([str(ids[row[0].strip()]), str(ids[row[1].strip()]), 1])
           else:
             error.append(row[0].strip()+', '+row[1].strip())
-            # print(str( (row[0].strip() in ids))+' '+ row[0].strip()+' '+row[1].strip()+' '+str((row[1].strip() in ids)))
-
 
     file2 = open(folder+lang+"/edges-"+lang+"-gephi.csv", "w", encoding="utf8")
     file2.writelines("\n".join(out))
